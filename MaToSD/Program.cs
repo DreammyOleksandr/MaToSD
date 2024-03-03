25fa968d98773d4673c6e670424880b5a8aab07d
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-03-03 19:04:52 +0200

    Main functionality is made (Regex)

--- MaToSD/Program.cs
+++ MaToSD/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // if (args.Length == 0)
+        // {
+        //     Console.WriteLine("Usage: markdownToHtml <input_file>");
+        //     return;
+        // }
+
+        string inputFile = "/Users/bondarenkooleksandr/C#Projects/MaToSD/MaToSD/TestMarkDown.md";
+
+        if (!File.Exists(inputFile))
+        {
+            Console.WriteLine("File does not exist.");
+            return;
+        }
+
+        string markdownText = File.ReadAllText(inputFile);
+        string htmlText = ConvertMarkdownToHtml(markdownText);
+        string outputFilePath = Path.ChangeExtension(inputFile, ".html");
+
+        File.WriteAllText(outputFilePath, htmlText);
+
+        Console.WriteLine($"HTML file successfully created: {outputFilePath}");
+    }
+
+
+    static string ConvertMarkdownToHtml(string markdownText)
+    {
+        string htmlText = markdownText;
+
+        htmlText = Regex.Replace(htmlText, @"\*\*(.*?)\*\*", "<b>$1</b>");
+        htmlText = Regex.Replace(htmlText, @"_(.*?)_", "<i>$1</i>");
+        htmlText = Regex.Replace(htmlText, @"```([\s\S]*?)```", "<pre>$1</pre>");
+        htmlText = Regex.Replace(htmlText, @"`(.*?)`", "<tt>$1</tt>");
+        htmlText = Regex.Replace(htmlText, @"\n{2,}", "</p>\n<p>");
+        htmlText = "<p>" + htmlText + "</p>";
+
+        return htmlText;
+    }
+}
