6d530318e0b618fd6788c1e26f86a98e4f7b3ad7
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-03-05 19:57:29 +0200

    Changing program structure

--- MaToSD/Program.cs
+++ MaToSD/Program.cs
@@ -1,49 +1,20 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // if (args.Length == 0)
-        // {
-        //     Console.WriteLine("Usage: markdownToHtml <input_file>");
-        //     return;
-        // }
+        string filePath = "/Users/bondarenkooleksandr/C#Projects/MaToSD/MaToSD/TestMarkDown.md";
 
-        string inputFile = "/Users/bondarenkooleksandr/C#Projects/MaToSD/MaToSD/TestMarkDown.md";
-
-        if (!File.Exists(inputFile))
+        if (!File.Exists(filePath))
         {
             Console.WriteLine("File does not exist.");
             return;
         }
-
-        string markdownText = File.ReadAllText(inputFile);
-        string htmlText = ConvertMarkdownToHtml(markdownText);
-        string outputFilePath = Path.ChangeExtension(inputFile, ".html");
-
-        File.WriteAllText(outputFilePath, htmlText);
-
-        Console.WriteLine($"HTML file successfully created: {outputFilePath}");
-    }
-
-
-    static string ConvertMarkdownToHtml(string markdownText)
-    {
-        if (Regex.IsMatch(markdownText, @"\*\*.*_.*\*\*|\*\*.*`.*\*\*|_.*\*\*.*_|_.*`.*_|`.*\*\*.*`|`.*_.*`"))
-        {
-            throw new InvalidOperationException("Nested markup detected. Cannot convert.");
-        }
-
-        string htmlText = markdownText;
         
-        htmlText = Regex.Replace(htmlText, @"\*\*(.*?)\*\*", "<b>$1</b>");
-        htmlText = Regex.Replace(htmlText, @"_(.*?)_", "<i>$1</i>");
-        htmlText = Regex.Replace(htmlText, @"```([\s\S]*?)```", "<pre>$1</pre>");
-        htmlText = Regex.Replace(htmlText, @"`(.*?)`", "<tt>$1</tt>");
-        htmlText = Regex.Replace(htmlText, @"\n{2,}", "</p>\n<p>");
-        htmlText = "<p>" + htmlText + "</p>";
+        List<string> lines = File.ReadAllLines(filePath).ToList();
         
-        return htmlText;
     }
 }
